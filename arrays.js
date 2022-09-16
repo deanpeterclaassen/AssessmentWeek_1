db254fb44054975dcf81cd3fa4399bf853a7ac8b
DeanPeter Claassen <deanpeter8162@gmail.com>
2022-09-16 10:45:20 -0400

    Arrays problems completed

--- arrays.js
+++ arrays.js
@@ -4,11 +4,13 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
+const colorCopy = faveColors.slice(0,3)
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+faveColors.push('purple')
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -19,7 +21,7 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+const middleNums = numbers.slice(1,4);
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +36,12 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+const answers = []
+for(let i = 0; i < bigOrSmallArray.length; i++){
+  if( bigOrSmallArray[i] >= 100){
+    answers.push('big')
+  }else if (bigOrSmallArray[i] <= 100){
+    answers.push('small')
+  }
+
+} 
